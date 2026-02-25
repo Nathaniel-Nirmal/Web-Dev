bac9cb615ed3328bde7ac89ce430688606eae770
Nathaniel Nirmal <na453@cornell.edu>
2026-02-25 15:17:19 -0800

    UseReducer hook in React

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,27 +1,24 @@
-import React from "react";
+import {useReducer} from "react";
+
+const reducer = (state, action) => {
+  if (action.type === 'buy_ingredients') return {money: state.money - 10};
+  if (action.type === 'sell_a_meal') return {money: state.money + 10};
+  return state;
+}
 
 function App() {
-  const [user, setUser] = React.useState([]);
+  const initialState = {money: 100};
+  const [state, dispatch] = useReducer(reducer, initialState)
 
-  const fetchData = () => {
-    fetch("https://randomuser.me/api/?results=1")
-    .then(response => response.json())
-    .then(data => setUser(data));
-  }
-
-  React.useEffect(() => {
-    fetchData();
-  }, []);
-
-  return Object.keys(user).length > 0 ? (
-    <div>
-      <h1>Data returned</h1>
-      <h2>First Name: {user.results[0].name.first}</h2>
-      <h2>Last Name: {user.results[0].name.last}</h2>
+  return (
+    <div className="App">
+      <h1>Wallet: {state.money}</h1>
+      <div>
+        <button onClick={() => dispatch({type: 'buy_ingredients'})}>Shopping for Veggies!</button>
+        <button onClick={() => dispatch({type: 'sell_a_meal'})}>Serve a meal to the customer</button>
+      </div>
     </div>
-  ) : (
-    <h1>Data pending...</h1>
-  );
+  )
 }
 
 export default App;
